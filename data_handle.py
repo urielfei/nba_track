1741f4b088e001ed3d6ffe1a53ff47240ac90da2
urielfei <uriel.feigelsohn@gmail.com>
2020-05-09 13:38:41 +0300

    Initial commit

---
+++ data_handle.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+from nba_api.stats.endpoints import boxscoreplayertrackv2
+from nba_api.stats.endpoints import leaguegamelog
+import time
+
+start = time.time()
+
+games = leaguegamelog.LeagueGameLog(season='2019-20').get_data_frames()[0]
+games = games['GAME_ID'].unique()
+
+df = pd.DataFrame()
+for index in games:
+    print(index)
+    raw = boxscoreplayertrackv2.BoxScorePlayerTrackV2(index).get_data_frames()[0]
+    df = df.append(raw)
+
+df.to_csv('cc.csv')
+print("--- %s seconds ---" % (time.time() - start))
